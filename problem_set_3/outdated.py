bb544f9f6cf3450f889bba0487ccea64de5d331a
Angel Sanchez <angelsanchez.ag@gmail.com>
2025-04-05 14:42:56 -0600

    Nothing major

--- problem_set_3/outdated.py
+++ problem_set_3/outdated.py
@@ -3,7 +3,9 @@ In the United States, dates are typically formatted in month-day-year order (MM/
 
 Fortunately, computers tend to use ISO 8601, an international standard that prescribes that dates should be formatted in year-month-day (YYYY-MM-DD) order, no matter the country, formatting years with four digits, months with two digits, and days with two digits, “padding” each with leading zeroes as needed.
 
-In a file called outdated.py, implement a program that prompts the user for a date, anno Domini, in month-day-year order, formatted like 9/8/1636 or September 8, 1636, wherein the month in the latter might be any of the values in the list below:
+In a file called outdated.py, 
+-- implement a program that prompts the user for a date, anno Domini, in month-day-year order, formatted like 9/8/1636 or September 8, 1636, 
+wherein the month in the latter might be any of the values in the list below:
 
 """
 
@@ -26,7 +28,9 @@ In a file called outdated.py, implement a program that prompts the user for a da
 """
 
 """
-Then output that same date in YYYY-MM-DD format. If the user’s input is not a valid date in either format, prompt the user again. Assume that every month has no more than 31 days; no need to validate whether a month has 28, 29, 30, or 31 days.
+-- Then output that same date in YYYY-MM-DD format. 
+-- If the user’s input is not a valid date in either format, prompt the user again. 
+-- Assume that every month has no more than 31 days; no need to validate whether a month has 28, 29, 30, or 31 days.
 """
 
 # Chat GPT: What are the basic building blocks of typing in Python?
