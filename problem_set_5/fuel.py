976813c2372000b5d1ed22fe5ce69bc24e5c37d2
Angel Sanchez <angelsanchez.ag@gmail.com>
2025-06-25 21:07:36 -0600

    Last update from today. Great day for learning, thanks!

--- problem_set_5/fuel.py
+++ problem_set_5/fuel.py
@@ -1,8 +1,31 @@
+"""
+
+In a file called fuel.py, reimplement Fuel Gauge from Problem Set 3, restructuring your code per the below, wherein:
+
+- convert expects a str in X/Y format as input, wherein each of X and Y is an integer, and returns that fraction as a percentage 
+    rounded to the nearest int between 0 and 100, inclusive. 
+- If X and/or Y is not an integer, or if X is greater than Y, then convert should raise a ValueError. 
+- If Y is 0, then convert should raise a ZeroDivisionError.
+
+- gauge expects an int and returns a str that is:
+    "E" if that int is less than or equal to 1,
+    "F" if that int is greater than or equal to 99,
+    and "Z%" otherwise, wherein Z is that same int.
+
+"""
 
 def main():
-    response = input('Fraction: ') # Request fraction
-    perc = convert(response) # Convert the fraction and store it in a variable called perc
-    print(gauge(perc)) # Print the output from gauge which is powered by the value from perc
+    try:
+        response = input("Fraction: ")
+        perc = convert(response)
+        print(gauge(perc))
+    except (ValueError, ZeroDivisionError) as e:
+        print(e)
+    
+    # My original response below
+    # response = input('Fraction: ') # Request fraction
+    # perc = convert(response) # Convert the fraction and store it in a variable called perc
+    # print(gauge(perc)) # Print the output from gauge which is powered by the value from perc
 
 # Goal: Return the response as a percentage
 def convert(fraction):
@@ -25,11 +48,10 @@ def convert(fraction):
                 # Ensure Y is not Zero
                 if y == 0:
                      raise ZeroDivisionError("Error: Y can't be zero.")
-                else:
-                     pass
+
                 
                 # Ensure X smaller than Y
-                if x < y:
+                if x <= y:
                      pass
                 else:
                      raise ValueError("Error: X can't be larger than Y")
@@ -41,9 +63,9 @@ def convert(fraction):
                 raise ValueError('Error: Not a fraction')
             
         except ValueError as e:
-            print(f'{e}')
+            raise
         except ZeroDivisionError as e:
-             print(f'{e}')
+             raise
 
 def gauge(percentage):
     if percentage <= 1:
@@ -52,7 +74,6 @@ def gauge(percentage):
          return f'F'
     else:
          return f'{percentage}%'
-
 
 if __name__ == "__main__":
     main()
